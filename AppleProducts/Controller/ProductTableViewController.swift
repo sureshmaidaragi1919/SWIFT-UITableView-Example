b9ba52f1f64276573aab8b70bde19a638d427972
Suresh Maidaragi <suresh.maidaragi@fridaymediagroup.com>
2018-08-08 17:31:36 +0530

    Move the cells between sections

--- AppleProducts/Controller/ProductTableViewController.swift
+++ AppleProducts/Controller/ProductTableViewController.swift
@@ -9,6 +9,17 @@
 // 2 - Create a subclass of UITableViewCell for new cell --done
 // 3 - Update the cell with UITableViewDataSource --progress
 
+
+// * Delete rows
+// 1. Edit button on the right
+// 2. Delete a row (in our data model and in our tableview -- UI)
+// 3. Nice animation -> move the tableview rows up
+
+// *Move the table view rows around
+// 1. Tell the table view that you want to be able to move rows around
+// 2. Update the data structure of the data model
+
+
 import UIKit
 
 class ProductTableViewController: UITableViewController {
@@ -21,11 +32,13 @@ class ProductTableViewController: UITableViewController {
         title = "Apple Products"
         
         
-        tableView.estimatedRowHeight = tableView.rowHeight
-        tableView.rowHeight = UITableViewAutomaticDimension
+        //tableView.estimatedRowHeight = tableView.rowHeight
+       // tableView.rowHeight = UITableViewAutomaticDimension
  
         tableView.separatorColor =  UIColor.red
-
+        
+        navigationItem.rightBarButtonItem = editButtonItem
+ 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -38,7 +51,6 @@ class ProductTableViewController: UITableViewController {
     //UITableViewDAta
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        
         
         return productLines.count
     }
@@ -68,11 +80,62 @@ class ProductTableViewController: UITableViewController {
         return cell
     }
     
+    // Multiple sections - title for HeaderInSection
+    
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
          let productLine = productLines[section]
         return productLine.name
     }
+    
+    
+    // Delete/Insert rows
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            // 1- delete the product from the product lines product arrays
+            
+            let productLine = productLines[indexPath.section]
+            productLine.products.remove(at: indexPath.row)
+            print("inside the delete row")
 
+            // 2 - Update the table view with the new data
+            
+           // tableView.reloadData() //bad way
+            
+            // animate smoothly instead loading everything again
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            tableView.allowsMultipleSelection = true
+        }
+        else if editingStyle == .insert{
+            let productLine = productLines[indexPath.section]
+            productLine.products.append(Product(titled: "I am idiot", description: "FUCKER HOW CAN YOU ASK ME THE DESCRIPTION ABOUT THE PRODUCT", imageName: "iphone"))
+            print("inside the insert row")
+ 
+            tableView.beginUpdates()
+            tableView.insertRows(at: [IndexPath.init(row: self.productLines.count-1, section: 0)], with: .automatic)
+            tableView.endUpdates()
+        }
+        
+    }
+    
+    
+    // Mark : - Move the cells
+    
+   override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let  productToMove = productLines[sourceIndexPath.section].products[sourceIndexPath.row]
+        
+        //move productToMove to destination products
+        productLines[destinationIndexPath.section].products.insert(productToMove, at: destinationIndexPath.row)
+        
+        //delete the productToMove from the source products
+        productLines[sourceIndexPath.section].products.remove(at: sourceIndexPath.row)
+    }
+    
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
